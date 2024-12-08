346f96b67b45b1f8e9710aec5071de56e73c7712
thinhndp <tligodsp@gmail.com>
2024-12-08 21:44:38 +0700

    update

---
+++ keyboards/sofle/keymaps/lyrrr/rules.mk
@@ -0,0 +1,10 @@
+TRI_LAYER_ENABLE = yes
+# Bootloader selection
+BOOTLOADER = caterina
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+CONSOLE_ENABLE = no
+RGBLIGHT_ENABLE = yes
+ENCODER_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
